7fe6a0d8a98c18980b6f0e716a8ba08949c7374e
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-08-20 15:24:10 +0900

    Update ImageUtils.py
    점들의 좌표가 있는 spot 이라는 list 가 사각형 영역 내에 존재하는지 확인하는 함수 isSpotInRect(rectPos, spot) 를 추가
    ==> TODO :: 사각형 인식이 이상하게 되는 패턴이 존재.  해결해야함.

--- ImageUtils.py
+++ ImageUtils.py
@@ -66,3 +66,80 @@ def draw_area(cvImg, pos_list, dotColor=(255, 0, 0), lineColor=(0, 255, 0)):
             cv2.line(cvImg, (start[0], start[1]), (end[0], end[1]), lineColor, 2)
 
     return cvImg
+
+
+# spots is Array
+# TODO: RESULT IS BOOLEAN ( TOTALLY IS IN AREA )
+def isSpotInRect(rectPos, spot):
+    if len(spot) == 0:
+        return False
+
+    copyList = list(rectPos)
+    x_arg_sort = []
+    for i in range(4):
+        arg = np.argmax(copyList, axis=0)[0]
+        x_arg_sort.append(arg)
+        copyList[arg] = [-1, -1]
+    x_arg_sort.reverse()
+
+    if rectPos[x_arg_sort[0]][1] > rectPos[x_arg_sort[1]][1]:
+        dump = x_arg_sort[0]
+        x_arg_sort[0] = x_arg_sort[1]
+        x_arg_sort[1] = dump
+
+    if rectPos[x_arg_sort[2]][1] < rectPos[x_arg_sort[3]][1]:
+        dump = x_arg_sort[2]
+        x_arg_sort[2] = x_arg_sort[3]
+        x_arg_sort[3] = dump
+
+    p1 = rectPos[x_arg_sort[0]]
+    p2 = rectPos[x_arg_sort[1]]
+    p3 = rectPos[x_arg_sort[2]]
+    p4 = rectPos[x_arg_sort[3]]
+
+    # High Low Low High
+    # y = (Ay-By)/(Ax-Bx)*( x - Ax ) + Ay
+    for i in range(0, len(spot)):
+
+        try:
+            # [xmin, ymax, xmax, ymin]
+            target = [(spot[i][0] + spot[i][2]) / 2, (spot[i][1] + spot[i][3]) / 2]
+
+            if p1[0] == p2[0]:
+                if not p1[0] < target[0]:
+                    continue
+            else:
+                # linear a > 0
+                if (p1[1] - p2[1]) / (p1[0] - p2[0]) > 0:
+                    if not ((p1[1] - p2[1]) / (p1[0] - p2[0]) * (target[0] - p1[0]) + p1[1]) < target[1]:
+                        continue
+                # linear a < 0
+                else:
+                    if not ((p1[1] - p2[1]) / (p1[0] - p2[0]) * (target[0] - p1[0]) + p1[1]) > target[1]:
+                        continue
+
+            if not ((p2[1] - p3[1]) / (p2[0] - p3[0]) * (target[0] - p2[0]) + p2[1]) > target[1]:
+                continue
+
+            if p3[0] == p4[0]:
+                if not p3[0] > target[0]:
+                    continue
+            else:
+                # linear a > 0
+                if (p3[1] - p4[1]) / (p3[0] - p4[0]) > 0:
+                    if not ((p3[1] - p4[1]) / (p3[0] - p4[0]) * (target[0] - p3[0]) + p3[1]) > target[1]:
+                        continue
+                # linear a < 0
+                else:
+                    if not ((p3[1] - p4[1]) / (p3[0] - p4[0]) * (target[0] - p3[0]) + p3[1]) < target[1]:
+                        continue
+
+            if not ((p4[1] - p1[1]) / (p4[0] - p1[0]) * (target[0] - p4[0]) + p4[1]) < target[1]:
+                continue
+
+        except Exception as e:
+            return False
+
+        return True
+
+    return False
